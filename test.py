e0a76d3fd761456dbc5b998a7709dadf2c1da168
benniehaelen <benniehaelen>
2024-10-15 01:41:22 +0000

    test
    test

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
 # Databricks notebook source
-
+# MAGIC %md-sandbox
+# MAGIC
+# MAGIC <img src= "https://cdn.oreillystatic.com/images/sitewide-headers/oreilly_logo_mark_red.svg"/>&nbsp;&nbsp;<font size="16"><b>AI, ML and GenAI in the Lakehouse<b></font></span>
+# MAGIC <img style="float: left; margin: 0px 15px 15px 0px;" src="https://learning.oreilly.com/covers/urn:orm:book:9781098139711/400w/" />  
+# MAGIC
+# MAGIC
+# MAGIC  
+# MAGIC   
+# MAGIC    Name:          chapter 03-04-Model Deployment
+# MAGIC  
+# MAGIC    Author:    Bennie Haelen
+# MAGIC    Date:      10-13-2024
+# MAGIC
+# MAGIC    Purpose:   This notebook registers a model in the Model Registry
+# MAGIC                  
+# MAGIC       An outline of the different sections in this notebook:
+# MAGIC         1 - Setup the run_id and the model name
+# MAGIC         2 - Register the model
+# MAGIC         3 - Move the model into production
+# MAGIC         
