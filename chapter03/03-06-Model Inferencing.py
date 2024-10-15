868c0213a6789a999ed3e8e20c5ccfbf709f9bee
benniehaelen <benniehaelen>
2024-10-15 01:57:50 +0000

    sdf

--- chapter03/03-06-Model Inferencing.py
+++ chapter03/03-06-Model Inferencing.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
 # MAGIC
+# MAGIC
 # MAGIC <img src= "https://cdn.oreillystatic.com/images/sitewide-headers/oreilly_logo_mark_red.svg"/>&nbsp;&nbsp;<font size="16"><b>AI, ML and GenAI in the Lakehouse<b></font></span>
 # MAGIC <img style="float: left; margin: 0px 15px 15px 0px;" src="https://learning.oreilly.com/covers/urn:orm:book:9781098139711/400w/" />  
 # MAGIC
@@ -29,7 +30,7 @@ import os
 # Set an environment variable 'DATABRICKS_TOKEN' with the value of your Databricks personal access token.
 # This token will be used for authenticating API requests to Databricks services (e.g., for model serving or running jobs).
 # The 'os.environ' dictionary allows you to set or access environment variables in the current session.
-# 
+
 
 # Note: This is a sensitive access token, so make sure you do not expose it in your code.
 # In a production environment, it's recommended to use more secure ways to manage sensitive information,
